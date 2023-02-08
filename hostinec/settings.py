523ad869f90ced99852b4dad31bc970ac8881461
Jakub <kubodemko@gmail.com>
2023-02-08 09:29:49 +0100

    settings change

--- hostinec/settings.py
+++ hostinec/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ujakuba.pythonanywhere.com']
 
 
 # Application definition
